a67b8f8b9d3b6fa5f1de49ca1b0177beb2373184
Moaath Alrajab <moaath@alrajab.net>
2020-11-23 00:24:22 -0500

    Initial commit

---
+++ app/src/main/java/com/leuenroo/draganddropviewdemo/MainActivity.java
@@ -0,0 +1,88 @@
+package com.leuenroo.draganddropviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MyStrtDrggngLstnr mStrtDrg;
+    MyEndDrgLstnr mEndDrg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mStrtDrg=new MyStrtDrggngLstnr();
+        mEndDrg=new MyEndDrgLstnr();
+
+        findViewById(R.id.btn_bck).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_dwn).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_fwd).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_up).setOnLongClickListener(mStrtDrg);
+
+        findViewById(R.id.btn_input1).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input2).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input3).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input4).setOnDragListener(mEndDrg);
+
+
+    }
+
+
+    private class MyStrtDrggngLstnr implements View.OnLongClickListener{
+
+        @Override
+        public boolean onLongClick(View v) {
+            WithDragShadow shadow= new WithDragShadow(v);
+            ClipData data= ClipData.newPlainText("","");;
+            v.startDrag(data,shadow,v,0);
+
+            return false;
+        }
+    }
+
+    private class MyEndDrgLstnr implements View.OnDragListener{
+
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            if(event.getAction()==DragEvent.ACTION_DROP){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    v.setBackground(((Button)event.getLocalState()).getBackground());
+                }
+
+            }
+
+            return true;
+        }
+    }
+
+    private class WithDragShadow extends View.DragShadowBuilder{
+
+        public WithDragShadow(View v){
+            super(v);
+        }
+
+        @Override
+        public void onDrawShadow(Canvas canvas) {
+            super.onDrawShadow(canvas);
+        }
+
+        @Override
+        public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
+            super.onProvideShadowMetrics(outShadowSize, outShadowTouchPoint);
+        }
+    }
+
+}
